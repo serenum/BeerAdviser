577896570df0875b089d6270cff8731dfa780d4b
serenum <saveliev_@rambler.ru>
2017-04-01 22:54:17 +0600

    Added three wigets and handlers for it

--- app/src/main/java/com/serenum/android/beeradviser/MainActivity.java
+++ app/src/main/java/com/serenum/android/beeradviser/MainActivity.java
@@ -1,7 +1,10 @@
 package com.serenum.android.beeradviser;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void getAdvice(View view){
+        TextView recommendations = (TextView) findViewById(R.id.recommendation_textView);
+        Spinner beerColor = (Spinner) findViewById(R.id.beer_color_spinner);
+        if((String.valueOf(beerColor.getSelectedItem()).equals("Light")))
+            recommendations.setText("Great choice!!");
+        else
+            recommendations.setText("FUUUU!!!");
     }
 }
